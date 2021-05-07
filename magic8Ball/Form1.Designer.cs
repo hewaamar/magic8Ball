0f1e5fa8f2296561909e10b83931c4a5e4afe341
Hewan Amare <hewaamar557@ed.amdsb.ca>
2021-05-07 14:58:35 -0400

    Add project files.

---
+++ magic8Ball/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace magic8Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button = new System.Windows.Forms.Button();
+            this.messageOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(254, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Think of a Question";
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.Color.Transparent;
+            this.button.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button.BackgroundImage")));
+            this.button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button.FlatAppearance.BorderSize = 0;
+            this.button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button.Location = new System.Drawing.Point(141, 115);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(470, 397);
+            this.button.TabIndex = 1;
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // messageOutput
+            // 
+            this.messageOutput.Location = new System.Drawing.Point(184, 586);
+            this.messageOutput.Name = "messageOutput";
+            this.messageOutput.Size = new System.Drawing.Size(397, 42);
+            this.messageOutput.TabIndex = 2;
+            this.messageOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 670);
+            this.Controls.Add(this.messageOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label messageOutput;
+    }
+}
+
---
+++ magic8Ball/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace magic8Ball
+{
+    public partial class Form1 : Form
+    {
+        // set up randomizer
+        Random randGen = new Random();
+        // set up random variable
+        int random = 0;
+      
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            random = randGen.Next(1, 7);
+
+            switch (random)
+            {
+                case 1:
+                    messageOutput.Text = "Yes";
+                    break;
+                case 2:
+                    messageOutput.Text = "No";
+                    break;
+                case 3:
+                    messageOutput.Text = "Maybe";
+                    break;
+                case 4:
+                    messageOutput.Text = "I don't know";
+                    break;
+                case 5:
+                    messageOutput.Text = "That's your decision";
+                    break;
+                case 6:
+                    messageOutput.Text = "No one knows";
+                    break;
+                case 7:
+                    messageOutput.Text = "That's enough questions for today";
+                    break;
+                default:
+                   messageOutput.Text = "Not a valid category";
+                    break;
+
+            }
+
+
+        }
+    }
+}
